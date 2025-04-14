6fce9f4713cc5817caa2c8b2be2b0b8b6c7b5616
Katembo1 <156650171+Katembo1@users.noreply.github.com>
2025-04-14 15:30:05 +0300

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -1,11 +1,1 @@
-test_code = """
-        int x;
-        x = 123;
-        int y;
-        y = 23;
-        int sum;
-        sum = y + x;
-        if(sum >= 0){
-        printf("sum is positive");
-        }
-        """
+test_code = input("Enter your c code please: \n")
